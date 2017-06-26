7384b687eaa8e57b1b8a3b78cd4c3cb92ea3c8d4
Jeremy <jeremyfs.moreau@gmail.com>
2017-06-26 11:41:12 +0200

    dernière version

--- rails_project/app/controllers/reservations_controller.rb
+++ rails_project/app/controllers/reservations_controller.rb
@@ -1,34 +1,32 @@
 class ReservationsController < ApplicationController
+  before_action :set_book
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
+
+
 
   # GET /reservations
   # GET /reservations.json
   def index
-    @book = Book.find(params[:book_id])
     @reservations = Reservation.includes(:user,:book)
   end
 
   # GET /reservations/1
   # GET /reservations/1.json
   def show
-    @book = Book.find(params[:book_id])
   end
 
   # GET /reservations/new
   def new
-    @book = Book.find(params[:book_id])
     @reservation = Reservation.new
   end
 
   # GET /reservations/1/edit
   def edit
-    @book = Book.find(params[:book_id])
   end
 
   # POST /reservations
   # POST /reservations.json
   def create
-    @book = Book.find(params[:book_id])
     if @book.stock <= 0
       redirect_to book_reservations_path(@book, @reservation), notice: 'Nous n\'avons plus le livre en stock'
     else
@@ -36,6 +34,7 @@ class ReservationsController < ApplicationController
       @reservation.user = current_user
       @reservation.book = @book
       @reservation.date_debut = DateTime.now
+      @reservation.rendu = false
       @book.stock = @book.stock-1
       @book.save
       if @reservation.save
@@ -51,7 +50,7 @@ class ReservationsController < ApplicationController
   def update
     set_reservation
     if @reservation.update(reservation_params)
-      redirect_to @reservation, notice: 'Reservation was successfully updated.'
+      redirect_to book_reservation_url notice: 'Reservation was successfully updated.'
     else
       render :edit
     end
@@ -60,7 +59,6 @@ class ReservationsController < ApplicationController
   # DELETE /reservations/1
   # DELETE /reservations/1.json
   def destroy
-    @book = Book.find(params[:book_id])
     @reservation.destroy
     @book.stock = @book.stock+1
     @book.save
@@ -69,7 +67,6 @@ class ReservationsController < ApplicationController
 
   def livre_rendu
     set_reservation
-    @book = Book.find(params[:book_id])
     @book.stock = @book.stock+1
     @book.save
     @reservation.rendu = true
@@ -83,10 +80,15 @@ class ReservationsController < ApplicationController
       @reservation = Reservation.find(params[:id])
     end
 
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def reservation_params
-      params.require(:reservation).permit(:date_fin)
+      params.require(:reservation).permit(:date_fin,:rendu)
     end
+
 
 
 end
